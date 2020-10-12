c08181ad553cf9843227f8aeabd1c6d4ab031591
s-show <s1shimz@gmail.com>
2020-10-12 23:52:00 +0900

    change probe offset.(For BLTouch)

--- firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
+++ firmware/V1.2/Marlin-2.0.x-SKR-Mini-E3-V1.2/Marlin/Configuration.h
@@ -946,7 +946,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET {-42, -12, -2.20 }
+#define NOZZLE_TO_PROBE_OFFSET {38.2, 0, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
